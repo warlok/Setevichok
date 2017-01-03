4d0ede7052e851a3c0151faf6ed1ef7d3c9eac50
Denys Bezprozvanniy <deanwhisper@gmail.com>
2017-01-03 19:19:56 +0200

    Added some changes

--- src/main/java/ua/dean/Launcher.java
+++ src/main/java/ua/dean/Launcher.java
@@ -8,7 +8,11 @@ import ua.dean.ui.MainApp;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Launcher {
 
@@ -18,7 +22,9 @@ public class Launcher {
     private static String TEST_DOMAIN2 = "wnet.ua";
 
     public static void main(String[] args) throws InterruptedException, TimeoutException, IOException {
-//        String routingTable = new ProcessExecutor().command("netstat", "-rn").readOutput(true).execute().outputUTF8();
+
+
+        String routingTable = new ProcessExecutor().command("netstat", "-rn").readOutput(true).execute().outputUTF8();
 //        String ipconfig = new ProcessExecutor().command("ipconfig", "/all").readOutput(true).execute().outputUTF8();
 //        String interfaces = new ProcessExecutor().command("netsh", "int", "ipv4", "show", "interfaces").readOutput(true)
 //                .execute()
@@ -29,26 +35,41 @@ public class Launcher {
 //                .outputUTF8();
 //        String nslookupHost2 = new ProcessExecutor().command("ping", TEST_DOMAIN2).readOutput(true).execute()
 //                .outputUTF8();
-//        List<String> dns = new LinkedList<>();
-//        List<String> gw = new LinkedList<>();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                MainApp.main(new String[]{});
+
+        Scanner scanner = new Scanner(routingTable);
+        Pattern defaultDelimiter = scanner.delimiter();
+        System.err.println(defaultDelimiter);
+        scanner.useDelimiter("\n");
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            if (str.contains("IPv4 Route Table")) {
+                IntStream.range(0,2).forEach( i -> scanner.next());
+                scanner.useDelimiter(defaultDelimiter);
             }
-            }).start();
-        for (int i = 0; i < 100000; i++) {
-            System.out.println(i);
+            System.out.println(scanner.next());
         }
-        MainApp.text.set("aaaaaaaaasds");
-        for (int i = 0; i < 100000; i++) {
-            System.out.println(i);
-        }
-        MainApp.text.set("dskljnDNFLSCVdncksdNcKL");
-        for (int i = 0; i < 100000; i++) {
-            System.out.println("");
-        }
-        MainApp.text.set("FFFFFFFFUUUUUUUUUUUfffffffffuuuuuuuu");
+        List<String> dns = new LinkedList<>();
+        List<String> gw = new LinkedList<>();
+
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                MainApp.main(new String[]{});
+//            }
+//            }).start();
+//        for (int i = 0; i < 100000; i++) {
+//            System.out.println(i);
+//        }
+//        MainApp.text.set("aaaaaaaaasds");
+//        for (int i = 0; i < 100000; i++) {
+//            System.out.println(i);
+//        }
+//        MainApp.text.set("dskljnDNFLSCVdncksdNcKL");
+//        for (int i = 0; i < 100000; i++) {
+//            System.out.println("");
+//        }
+//        MainApp.text.set("FFFFFFFFUUUUUUUUUUUfffffffffuuuuuuuu");
 
 
     }
