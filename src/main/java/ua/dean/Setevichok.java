35a53e67178c007ec136511d8088e05d80eb691d
Denys Bezprozvannyy <deanwhisper@gmail.com>
2017-01-10 13:26:13 +0200

    Algorithm implemented

--- src/main/java/ua/dean/Setevichok.java
+++ src/main/java/ua/dean/Setevichok.java
@@ -7,28 +7,16 @@ import org.zeroturnaround.exec.ProcessExecutor;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.TimeoutException;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
-public class Launcher {
+public class Setevichok {
 
-    private static String TEST_IP1 = "8.8.8.23";
-    private static String TEST_IP2 = "8.8.4.4";
-    private static String TEST_DOMAIN1 = "google.com";
-    private static String TEST_DOMAIN2 = "wnet.ua";
+    public static String TEST_IP1 = "8.8.8.23";
+    public static String TEST_IP2 = "8.8.4.4";
+    public static String TEST_DOMAIN1 = "google.com";
+    public static String TEST_DOMAIN2 = "wnet.ua";
 
-    public static void main(String[] args) throws InterruptedException, TimeoutException, IOException {
-
-//        String nslookupHost1 = new ProcessExecutor().command("nslookup", TEST_DOMAIN1).readOutput(true).execute()
-//                .outputUTF8();
-//        String nslookupHost2 = new ProcessExecutor().command("ping", TEST_DOMAIN2).readOutput(true).execute()
-//                .outputUTF8();
-
-        System.out.println(nsLookupIsSuccess(TEST_DOMAIN2));
-
-    }
-
-    private static boolean nsLookupIsSuccess(String domain) throws InterruptedException, TimeoutException,
+    public boolean nsLookupIsSuccess(String domain) throws InterruptedException, TimeoutException,
             IOException {
         String pingResult = new ProcessExecutor().command("ping", "-n", "1", domain).readOutput(true).execute()
                 .outputUTF8();
@@ -36,13 +24,13 @@ public class Launcher {
         return true;
     }
 
-    private static boolean pingIsSuccess(String ipAddress) throws InterruptedException, TimeoutException, IOException {
+    public boolean pingIsSuccess(String ipAddress) throws InterruptedException, TimeoutException, IOException {
         String pingResult = new ProcessExecutor().command("ping", ipAddress).readOutput(true).execute().outputUTF8();
         if ( pingResult.split("\r\n").length == 11 ) return true;
         return false;
     }
 
-    private static List<String> getGateways() throws InterruptedException, TimeoutException, IOException {
+    public List<String> getGateways() throws InterruptedException, TimeoutException, IOException {
         String routingTable = new ProcessExecutor().command("netstat", "-rn").readOutput(true).execute()
                 .outputUTF8();
         List<String> gw = new LinkedList<>();
@@ -59,7 +47,7 @@ public class Launcher {
         return gw;
     }
 
-    private static Set<String> getDnsServers() throws InterruptedException, TimeoutException, IOException {
+    public Set<String> getDnsServers() throws InterruptedException, TimeoutException, IOException {
         String dnsServers = new ProcessExecutor().command("netsh", "int", "ipv4","show","dnsservers").readOutput(true)
                 .execute().outputUTF8();
         Set<String> dnss = new HashSet<>();
@@ -96,7 +84,7 @@ public class Launcher {
         return dnss;
     }
 
-    private static List<String> getUpInterfaces() throws InterruptedException, TimeoutException, IOException {
+    public List<String> getUpInterfaces() throws InterruptedException, TimeoutException, IOException {
         String interfaces = new ProcessExecutor().command("netsh", "int", "ipv4", "show", "interfaces").readOutput(true)
                 .execute().outputUTF8();
         List<String> ifaces = new LinkedList<>();
@@ -109,6 +97,13 @@ public class Launcher {
                 IntStream.range(0, 4).forEach(i -> stringScaner.next());
                 stringScaner.useDelimiter("\r");
                 ifaces.add(stringScaner.next().trim());
+            }
+        }
+
+        Iterator<String> iterator = ifaces.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().contains("Loopback")) {
+                iterator.remove();
             }
         }
         return ifaces;
--- src/main/java/ua/dean/ui/MainApp.java
+++ src/main/java/ua/dean/ui/MainApp.java
@@ -1,9 +1,6 @@
 package ua.dean.ui;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
@@ -11,24 +8,83 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
+import ua.dean.Setevichok;
 
 import java.io.IOException;
-import java.util.UUID;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeoutException;
 
 public class MainApp extends Application {
 
-    public static StringProperty text = new SimpleStringProperty("111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+    private static StringProperty text = new SimpleStringProperty("Diagnostic is started");
+    private static StringBuilder sb = new StringBuilder("Getting list of gateways...");
 
-    public static void main(String[] args) {
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), ev -> {
-            text.set(UUID.randomUUID().toString());
-        }));
-        timeline.setCycleCount(3);
-        timeline.play();
-        launch(args);
+    public static void main(String[] args) throws InterruptedException, IOException, TimeoutException {
+        new Thread(() -> launch(args)).start();
+        Setevichok setevichok = new Setevichok();
+        updateText();
+        List<String> gateways =  setevichok.getGateways();
+        sb.append("done\n");
+        updateText();
+        if (gateways.isEmpty()) {
+            sb = new StringBuilder("Не доступен шлюз, обратитесь к админисратору");
+            updateText();
+            return;
+        }
+        sb.append("Getting list of UP interfaces...");
+        updateText();
+        List<String> interfaces = setevichok.getUpInterfaces();
+        if (interfaces.isEmpty()) {
+            sb = new StringBuilder("Нет активных интерфейсов, обратитесь к админисратору");
+            updateText();
+            return;
+        }
+        sb.append("done\nPing Gateways...");
+        updateText();
+        boolean gwStatus = false;
+        for (String gw : gateways) {
+            boolean status = setevichok.pingIsSuccess(gw);
+            if (status == true) gwStatus = true;
+        }
+        if (gwStatus == false) {
+            sb = new StringBuilder("Не доступен шлюз, обратитесь к админисратору");
+            return;
+        }
+        sb.append("done\nPing External resources...");
+        updateText();
+        if (!setevichok.pingIsSuccess(setevichok.TEST_IP1)) {
+            if (!setevichok.pingIsSuccess(setevichok.TEST_IP2)) {
+                sb = new StringBuilder("Не работает интернет, отправьте запрос в провайдер");
+                updateText();
+                return;
+            }
+        }
+        sb.append("done\nTry to lookup domain names...");
+        if (setevichok.nsLookupIsSuccess(setevichok.TEST_DOMAIN1) || setevichok.nsLookupIsSuccess(setevichok.TEST_DOMAIN2)) {
+                sb = new StringBuilder("Интернет работает. Перезагрузитесь или обратитесь к администратору");
+                updateText();
+                return;
+        }
+        sb.append("done\nPing DNS...");
+        updateText();
+        Set<String> dnsServers =  setevichok.getDnsServers();
+        boolean dnsStatus = false;
+        for (String dns : dnsServers) {
+            boolean status = setevichok.pingIsSuccess(dns);
+            if (status == true) dnsStatus = true;
+        }
+        if (dnsStatus == true) {
+            sb = new StringBuilder("Не исправны или неверные DNS-сервера, обратитесь к администратору для исправления настроек");
+            updateText();
+            return;
+        }
+        sb = new StringBuilder("Не доступны DNS-сервера, обратитесь к администратору для исправления настроек");
+        updateText();
+    }
 
-
+    private static void updateText() {
+        text.set(sb.toString());
     }
 
     @Override
